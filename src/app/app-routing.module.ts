9ec10cd91ffd41c17bcde06850ab2e135b710b6a
PaiSoeMoe <paisoe1991@gmail.com>
2020-05-07 00:10:15 -0400

    img-width fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { IndexComponent } from './index/index.component';
 import { ListingComponent } from './listing/listing.component';
 import { ViewHouseComponent } from './view-house/view-house.component';
+import { NewComponent } from './new/new.component';
 
 
 
 const routes: Routes = [
   { path: "haha", component: ViewHouseComponent },
   { path: "listing", component: ListingComponent },
-  { path: "", component: IndexComponent }
+  { path: "news", component: NewComponent },
+  { path: "", component: IndexComponent },
+
 ];
 
 @NgModule({
